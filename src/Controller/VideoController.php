e36f5d28470edb04fb1066a694a8da77953a2333
espirito237 <adouessono@yahoo.fr>
2020-09-23 19:58:39 +0200

    searchbar in all pages and errormessage if videotitle is not found

--- src/Controller/VideoController.php
+++ src/Controller/VideoController.php
@@ -66,42 +66,101 @@ class VideoController extends AbstractController
     /**
      * @Route("/videos/{category}", name="videobycategory")
      */
-    public function videobyCategory(VideosRepository $repository, $category, CategoryRepository $repo)
+    public function videobyCategory(VideosRepository $repository, $category, CategoryRepository $repo, Request $request)
     {   
+        
+        $searchForm = $this->createForm(VideoSearchType::class);
+        $searchForm->handleRequest($request);
+        
+        $donnees = $repository->findAll();
+ 
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+ 
+            $videoTitle = $searchForm->getData()->getVideoTitle();
+
+            $donnees = $repository->search($videoTitle);
+
+
+            if ($donnees == null) {
+                
+                $errormessage = "Aucune vidéo trouvée avec ce titre";
+                $videos = $repository->search($videoTitle);
+                return $this->render('video/showresult.html.twig', [
+                "errormessage" => $errormessage,
+                "searchForm" => $searchForm->createView(),
+                "videos" => $videos
+
+                ]);
+           
+            }
+
+            else {
+
+                $videos = $repository->search($videoTitle);
+                return $this->render('video/showresult.html.twig', [
+                "videos" => $videos,
+                "searchForm" => $searchForm->createView()
+        ]);
+
+
+            }
+
+        }
+        
         $videos = $repository->getVideoByCategory($category);
         $categories = $repo->findAll();
         return $this->render('video/index.html.twig', [
             "videos" => $videos,
-            "categories" => $categories
+            "categories" => $categories,
+            "searchForm" => $searchForm->createView()
            
         ]);
     }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     /**
      * @Route("/listmovies/{category}", name="moviebycategory")
      */
-    public function moviebyCategory(VideosRepository $repository, $category, CategoryRepository $repo)
+    public function moviebyCategory(VideosRepository $repository, $category, CategoryRepository $repo, Request $request)
     {   
+
+        $searchForm = $this->createForm(VideoSearchType::class);
+        $searchForm->handleRequest($request);
+        
+        $donnees = $repository->findAll();
+ 
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+ 
+            $videoTitle = $searchForm->getData()->getVideoTitle();
+
+            $donnees = $repository->search($videoTitle);
+
+
+            if ($donnees == null) {
+                
+                echo "la vie est belle";
+           
+            }
+
+            else {
+
+                $videos = $repository->search($videoTitle);
+                return $this->render('video/showresult.html.twig', [
+                "videos" => $videos,
+                "searchForm" => $searchForm->createView()
+        ]);
+
+
+            }
+
+        }
+
         $videos = $repository->getVideoByCategory($category);
         $categories = $repo->findAll();
         return $this->render('video/listmovie.html.twig', [
             "videos" => $videos,
-            "categories" => $categories
+            "categories" => $categories,
+            "searchForm" => $searchForm->createView()
            
         ]);
     }
@@ -113,19 +172,51 @@ class VideoController extends AbstractController
     * @ParamConverter("video", options={"mapping": {"id" : "id"}})
     * @ParamConverter("category", options={"mapping": {"idcategory" : "id"}})
     */
-    public function movie(VideosRepository $repository, Videos $video, Category $category, $id, EntityManagerInterface $entity)
+    public function movie(VideosRepository $repository, Videos $video, Category $category, $id, EntityManagerInterface $entity, Request $request)
     {   
         $nbreview = $video->getViews();
         $nbreview++;
         $video->setViews($nbreview);
         $entity->persist($video);
         $entity->flush();
+
+        $searchForm = $this->createForm(VideoSearchType::class);
+        $searchForm->handleRequest($request);
         
+        $donnees = $repository->findAll();
+ 
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+ 
+            $videoTitle = $searchForm->getData()->getVideoTitle();
+
+            $donnees = $repository->search($videoTitle);
+
+
+            if ($donnees == null) {
+                
+                echo "la vie est belle";
+           
+            }
+
+            else {
+
+                $videos = $repository->search($videoTitle);
+                return $this->render('video/showresult.html.twig', [
+                "videos" => $videos,
+                "searchForm" => $searchForm->createView()
+        ]);
+
+
+            }
+
+        }
+
         $videos = $repository->showVideoByCategory($category, $id);
         return $this->render('video/singlemovie.html.twig', [
             "videos" => $videos,
             "video" => $video,
-            "category" => $category
+            "category" => $category,
+            "searchForm" => $searchForm->createView()
         ]);
     }
 
@@ -133,16 +224,48 @@ class VideoController extends AbstractController
     /**
     * @Route("/listvideo", name="allvideos")
     */
-    public function listVideo(VideosRepository $repository, CategoryRepository $repo)
+    public function listVideo(VideosRepository $repository, CategoryRepository $repo, Request $request)
     {   
+        
+        $searchForm = $this->createForm(VideoSearchType::class);
+        $searchForm->handleRequest($request);
+        
+        $donnees = $repository->findAll();
+ 
+        if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+ 
+            $videoTitle = $searchForm->getData()->getVideoTitle();
+
+            $donnees = $repository->search($videoTitle);
+
+
+            if ($donnees == null) {
+                
+                echo "la vie est belle";
+           
+            }
+
+            else {
+
+                $videos = $repository->search($videoTitle);
+                return $this->render('video/showresult.html.twig', [
+                "videos" => $videos,
+                "searchForm" => $searchForm->createView()
+        ]);
+
+
+            }
+
+        }
+        
         $videos = $repository->findAll(Category::class);
         $categories = $repo->findAll();
 
         return $this->render('video/listmovie.html.twig', [
             "categories" => $categories,
-            "videos" => $videos
+            "videos" => $videos,
+            "searchForm" => $searchForm->createView()
         ]);
     }
-
 
 }
