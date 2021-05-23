9912ef58ddf369dc77251ee7716efdc48fbbd6da
espirito237 <adouessono@yahoo.fr>
2021-05-23 10:35:05 +0200

    update

--- migrations/Version20210523083053.php
+++ migrations/Version20210523083053.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20210522033050 extends AbstractMigration
+final class Version20210523083053 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,16 +20,12 @@ final class Version20210522033050 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE abonnements ADD CONSTRAINT FK_4788B767C325A696 FOREIGN KEY (abonne_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE abonnements ADD CONSTRAINT FK_4788B767633E2BBF FOREIGN KEY (abonnements_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE avatar ADD CONSTRAINT FK_1677722FA76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE abonnements DROP FOREIGN KEY FK_4788B767C325A696');
-        $this->addSql('ALTER TABLE abonnements DROP FOREIGN KEY FK_4788B767633E2BBF');
         $this->addSql('ALTER TABLE avatar DROP FOREIGN KEY FK_1677722FA76ED395');
     }
 }
