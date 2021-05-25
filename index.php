7e891e5bbb9d7c15fc8fc8145ecf515b83842353
espirito237 <adouessono@yahoo.fr>
2021-05-25 10:46:11 +0200

    update

--- index.php
+++ index.php
@@ -4,9 +4,9 @@ use App\Kernel;
 use Symfony\Component\ErrorHandler\Debug;
 use Symfony\Component\HttpFoundation\Request;
 
-require dirname(__DIR__).'/config/bootstrap.php';
 
-ini_set('display_errors',1);
+
+require dirname(__DIR__).'/config/bootstrap.php';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
